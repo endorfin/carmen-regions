bcdf6b6528fff982c1223c0359deb655224f5a2c
René Kersten <rene.kersten@gmail.com>
2018-08-15 12:02:57 +0200

    add german translation

--- spec/carmen/region_spec.rb
+++ spec/carmen/region_spec.rb
@@ -14,4 +14,15 @@ describe Carmen::Country do
       expect(@us.sub_region).to eq("Northern America")
     end
   end
+
+  describe "localization" do
+    before do
+      Carmen.i18n_backend.locale = :de
+      @de = Carmen::Country.coded('de')
+    end
+
+    it "has a german translation" do
+      expect(@de.region).to eq("Europa")
+    end
+  end
 end
